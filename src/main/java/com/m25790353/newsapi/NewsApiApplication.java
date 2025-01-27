62b7992cb5e9a4c72a7dbf21c39b2b827361ffd4
Developer <michael@whitmoreprice.com>
2025-01-27 11:41:16 +0000

    initial commit

---
+++ src/main/java/com/m25790353/newsapi/NewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.m25790353.newsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/m25790353/newsapi/NewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m25790353.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
