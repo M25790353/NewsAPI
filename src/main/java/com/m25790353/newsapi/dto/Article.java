59b94d842f7d220547873a0afedf2fccacfa7b47
Developer <michael@whitmoreprice.com>
2025-02-24 14:52:28 +0000

    added version numbring into Maven pom file

--- src/main/java/com/m25790353/newsapi/dto/Article.java
+++ src/main/java/com/m25790353/newsapi/dto/Article.java
@@ -3,6 +3,7 @@ package com.m25790353.newsapi.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Generated;
 
 @Builder
 @Data
@@ -17,4 +18,70 @@ public class Article {
     private String urtToImage;
     private String publishedAt;
     private String content;
+
+
+    public Source getSource() {
+        return this.source;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getUrtToImage() {
+        return this.urtToImage;
+    }
+
+    public String getPublishedAt() {
+        return this.publishedAt;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setSource(final Source source) {
+        this.source = source;
+    }
+
+    public void setAuthor(final String author) {
+        this.author = author;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    public void setUrtToImage(final String urtToImage) {
+        this.urtToImage = urtToImage;
+    }
+
+    public void setPublishedAt(final String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
+
 }
